9b42b1d8e9dde3006b8c5cbd15658dee17479de8
Joshua Mervine <jmervine@users.noreply.github.com>
2025-10-27 20:25:37 -0700

    Update version to 2.0.0-build1

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version is the current application version
-const Version = "2.0.0"
+const Version = "2.0.0-build1"
 
 // GetVersion returns the current version
 func GetVersion() string {
